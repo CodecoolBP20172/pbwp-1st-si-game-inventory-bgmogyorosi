a70a6652b924b4f45b7a50f41513bb04e27e574d
Balint Mogyorosi <bg.mogyorosi@gmail.com>
2017-06-19 21:48:26 +0200

    display_inventory created

--- gameInventory.py
+++ gameInventory.py
@@ -1,9 +1,13 @@
-# This is the file where you must work. Write code in the functions, create new functions, 
+# This is the file where you must work. Write code in the functions, create new functions,
 # so they work according to the specification
+
 
 # Displays the inventory.
 def display_inventory(inventory):
-    pass
+    max_length = len(str(max(list(inventory.values()))))
+    for item in inventory:
+        print('%{}d {}'.format(max_length, item) % inventory[item])
+    print('Total number of items: {}'.format(sum(list(inventory.values()))))
 
 
 # Adds to the inventory dictionary a list of items from added_items.
@@ -11,11 +15,11 @@ def add_to_inventory(inventory, added_items):
     pass
 
 
-# Takes your inventory and displays it in a well-organized table with 
+# Takes your inventory and displays it in a well-organized table with
 # each column right-justified. The input argument is an order parameter (string)
 # which works as the following:
 # - None (by default) means the table is unordered
-# - "count,desc" means the table is ordered by count (of items in the inventory) 
+# - "count,desc" means the table is ordered by count (of items in the inventory)
 #   in descending order
 # - "count,asc" means the table is ordered by count in ascending order
 def print_table(inventory, order=None):
@@ -23,7 +27,7 @@ def print_table(inventory, order=None):
 
 
 # Imports new inventory items from a file
-# The filename comes as an argument, but by default it's 
+# The filename comes as an argument, but by default it's
 # "import_inventory.csv". The import automatically merges items by name.
 # The file format is plain text with comma separated values (CSV).
 def import_inventory(inventory, filename="import_inventory.csv"):
@@ -32,7 +36,7 @@ def import_inventory(inventory, filename="import_inventory.csv"):
 
 # Exports the inventory into a .csv file.
 # if the filename argument is None it creates and overwrites a file
-# called "export_inventory.csv". The file format is the same plain text 
+# called "export_inventory.csv". The file format is the same plain text
 # with comma separated values (CSV).
 def export_inventory(inventory, filename="export_inventory.csv"):
     pass
